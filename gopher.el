bda2b1790ce695c3d9ea10167fda4bb208e54745
Matthew Snyder <matthew.c.snyder@gmail.com>
2011-09-30 12:58:50 -0400

    0.0.1 release.
    Implementation of most major features.

--- gopher.el
+++ gopher.el
@@ -1,11 +1,125 @@
-(defun gopher-goto-url (url)
+;;; gopher.el --- easily access and navigate Gopher servers
+
+;; Copyright 2011 Google via Matthew Snyder
+
+;; Author: Matthew Snyder <matthew.c.snydere@gmail.com>
+;; URL: http://github.com/ardekantur/gopher.el
+;; Version: 0.0.1
+
+;; This file is not part of GNU Emacs.
+
+;;; Commentary:
+
+;; gopher.el allows you to navigate Gopher servers.
+
+;; "M-x gopher" prompts you for an address. <TAB> and <M-TAB> navigate
+;; between links on a directory listing, while <[> and <]> navigate
+;; between text documents. <RET> opens the link at the cursor's
+;; position. There's no history yet, but you can navigate upwards the
+;; directory tree with <u>.
+
+(require 'cl)
+
+(defconst gopher-available-content-types
+  '(("0" . plain-text)
+    ("1" . directory-listing)
+    ("i" . informational-message)
+    ("g" . gif)
+    ("I" . generic-image)
+    ("7" . search-query)))
+
+(defconst gopher-extra-network-arguments
+  '((gif . (:coding binary))
+    (generic-image . (:coding binary))))
+
+(defconst gopher-faces
+  '((directory-listing . font-lock-builtin-face)
+    (informational-message . font-lock-comment-face)
+    (gif . font-lock-variable-name-face)
+    (generic-image . font-lock-string-face)))
+
+(defvar gopher-buffer-name "*gopher*")
+
+(defun gopher-get-matching (function content-type)
+  (let ((name (intern (concat "gopher-" function "-" (symbol-name content-type)))))
+    (if (fboundp name)
+        name
+      (intern (concat "gopher-" function)))))
+
+(defun gopher-refresh-current-address ()
+  (interactive)
+  (apply 'gopher-goto-url gopher-current-address))
+
+(defun gopher-get-content-type (line-data)
+  (let ((content-type (assoc (getf line-data :item-type) gopher-available-content-types)))
+    (if content-type
+        (cdr content-type)
+      nil)))
+
+(defun gopher-get-face (content-type)
+  (let ((face (assoc content-type gopher-faces)))
+    (if face
+        (cdr face)
+      nil)))
+
+(defun gopher-get-extra-network-args (content-type)
+  (let ((args (assoc content-type gopher-extra-network-arguments)))
+    (if args
+        (cdr args)
+      nil)))
+
+(defun gopher (address)
   (interactive "MGopher URL: ")
-  (setq gopher-current-network-process (make-network-process :name "gopher" :buffer "*gopher*" :host url :service 70 :filter 'gopher-server-filter :sentinel 'gopher-server-sentinel)
+  (let* ((split-address (split-string (replace-regexp-in-string "^gopher:\/\/" "" address) "/"))
+         (url (car split-address))
+         (selector (mapconcat 'identity (cdr split-address) "/")))
+    (if (< (length selector) 1)
+        (gopher-goto-url url nil)
+      (gopher-goto-url url (concat "/" selector)))))
+
+(defun gopher-goto-url (&optional url selector content-type search-argument)
+  (interactive)
+  (if (get-buffer gopher-buffer-name)
+      (kill-buffer gopher-buffer-name))
+  (if (not content-type)
+      (setq content-type 'directory-listing))
+  (setq gopher-network-args (append (list
+                                     :name "gopher" 
+                                     :buffer gopher-buffer-name 
+                                     :host url 
+                                     :service 70 
+                                     :filter (gopher-get-matching "filter" content-type)
+                                     :sentinel (gopher-get-matching "sentinel" content-type))
+                                    (gopher-get-extra-network-args content-type)))
+  (setq gopher-current-network-process (apply 'make-network-process gopher-network-args)
         gopher-line-fragment nil)
-  (process-send-string gopher-current-network-process "\n")
-  (set-window-buffer (selected-window) "*gopher*")
-  (with-current-buffer "*gopher*"
+  (process-send-string gopher-current-network-process (gopher-prepare-request selector search-argument))
+  (gopher-prepare-buffer url selector))
+
+(defun gopher-prepare-request (selector search-argument)
+  (cond
+   ((and selector search-argument) (format "%s\t%s\r\n" selector search-argument))
+   (selector (format "%s\r\n" selector))
+   (t "\r\n")))
+  
+(defun gopher-prepare-buffer (url selector)
+  (set-window-buffer (selected-window) gopher-buffer-name)
+  (with-current-buffer gopher-buffer-name
+    (gopher-mode)
+    (setq gopher-current-address (list url selector)
+          gopher-current-data nil
+          line-spacing 3)
     (insert "\n\n")))
+
+(defun gopher-format-address (address)
+  (let ((url (nth 0 address))
+        (selector (nth 1 address)))
+    (cond
+     ((and selector
+           (not (zerop (length selector)))
+           (string= "/" (substring selector 0 1))) (format "%s%s" url selector))
+     (selector (format "%s/%s" url selector))
+     (t url))))
 
 (defun gopher-process-line (line)
   (let* ((lineparts (split-string line "\t"))
@@ -20,69 +134,170 @@
           :hostname hostname
           :port port)))
 
-(defun gopher-server-filter (proc string)
-  (let* ((lines (split-string string "\n"))
-         (first-line (pop lines))
-         (last-line (car (last lines))))
-    (with-current-buffer "*gopher*"
-      (if gopher-line-fragment
-          (insert (gopher-display-line (concat gopher-line-fragment first-line)))
-        (insert (gopher-display-line first-line)))
-      (mapc (lambda (line)
-              (if (string-match "$" line)
-                  (insert (gopher-display-line line))))
-            lines)
-      (if (string-match "$" last-line)
-          (setq gopher-line-fragment nil)
-        (setq gopher-line-fragment last-line)))))
+(defun gopher-filter (proc string)
+  (with-current-buffer gopher-buffer-name
+    (insert string)))
+
+(defun gopher-remove-dos-eol ()
+  "Do not show ^M in files containing mixed UNIX and DOS line endings."
+  (interactive)
+  (setq buffer-display-table (make-display-table))
+  (aset buffer-display-table ?\^M []))
+
+(defun gopher-filter-gif (proc string)
+  (with-current-buffer gopher-buffer-name
+    (setq gopher-current-data (concat gopher-current-data string))))
+
+(defun gopher-filter-generic-image (proc string)
+  (with-current-buffer gopher-buffer-name
+    (setq gopher-current-data (concat gopher-current-data string))))
+
+(defun gopher-filter-directory-listing (proc string)
+  (with-current-buffer gopher-buffer-name
+    (setq gopher-current-data (concat gopher-current-data string))))
 
 (defun gopher-display-line (line)
-  (if (zerop (length line))
+  (if (or 
+       (zerop (length line))
+       (string-match "^\.$" line))
       ""
-    (concat "     " (gopher-format-line (gopher-process-line line)) "\n")))
-
-(defun gopher-directory-menu-listing-p (line-data)
-  (string= (getf line-data :item-type) "1"))
-
-(defun gopher-plain-text-p (line-data)
-  (string= (getf line-data :item-type) "0"))
-
-(defun gopher-informational-message-p (line-data)
-  (string= (getf line-data :item-type) "i"))
+    (let* ((line-data (gopher-process-line line))
+           (indent (apply 'propertize "     " line-data)))
+      (concat indent (gopher-format-line line-data) "\n"))))
 
 (defun gopher-format-line (line-data)
-  (cond 
-   ((gopher-directory-menu-listing-p line-data) (gopher-formatted-directory-menu-listing-text line-data))
-   ((gopher-plain-text-p line-data) (gopher-formatted-plain-text-text line-data))
-   ((gopher-informational-message-p line-data) (gopher-formatted-informational-message-text line-data))
-   (t (getf line-data :display-string))))
+  (let ((content-type (gopher-get-content-type line-data)))
+    (if (and content-type (gopher-get-face content-type))
+        (propertize (getf line-data :display-string) 
+                    'face (gopher-get-face content-type))
+      (getf line-data :display-string))))
 
-(defun gopher-formatted-directory-menu-listing-text (line-data)
-  (propertize (getf line-data :display-string) 'face 'gopher-directory-menu-listing-face))
-
-(defun gopher-formatted-informational-message-text (line-data)
-  (propertize (getf line-data :display-string) 'face 'gopher-informational-message-face))
-
-(defun gopher-formatted-plain-text-text (line-data)
-  (propertize (getf line-data :display-string) 'face 'gopher-plain-text-face))
-
-(defface gopher-plain-text-face
-  '((t :foreground "black"))
-  "Face for Gopher directory plain text."
-  :group 'gopher-faces)
-
-(defface gopher-directory-menu-listing-face
-  '((t :foreground "blue" :weight bold))
-  "Face for Gopher directory menu listings."
-  :group 'gopher-faces)
-
-(defface gopher-informational-message-face
-  '((t :foreground "gray57"))
-  "Face for Gopher informational messages."
-  :group 'gopher-faces)
-  
-(defun gopher-server-sentinel (proc msg)
+(defun gopher-sentinel (proc msg)
   (when (string= msg "connection broken by remote peer\n")
-    (message (format "client %s has quit" proc))))
-    ;; (with-current-buffer "*gopher*"
-    ;;   (setq buffer-read-only t))))
+    (with-current-buffer gopher-buffer-name
+      (gopher-finish-buffer))))
+
+(defun gopher-sentinel-directory-listing (proc msg)
+  (when (string= msg "connection broken by remote peer\n")
+    (with-current-buffer gopher-buffer-name
+      (let* ((lines (split-string gopher-current-data "\r\n")))
+        (mapc (lambda (line) (insert (gopher-display-line line))) lines))
+      (gopher-finish-buffer))))
+
+(defalias 'gopher-sentinel-search-query 'gopher-sentinel-directory-listing)
+(defalias 'gopher-filter-search-query 'gopher-filter-directory-listing)
+
+(defun gopher-sentinel-plain-text (proc msg)
+  (when (string= msg "connection broken by remote peer\n")
+    (with-current-buffer gopher-buffer-name
+      (gopher-finish-buffer))))
+
+(defun gopher-sentinel-gif (proc msg)
+  (when (string= msg "connection broken by remote peer\n")
+    (with-current-buffer gopher-buffer-name
+      (insert "     ")
+      (insert-image (create-image gopher-current-data 'gif 'data))
+      (gopher-finish-buffer))))
+
+(defun gopher-sentinel-generic-image (proc msg)
+  (when (string= msg "connection broken by remote peer\n")
+    (with-current-buffer gopher-buffer-name
+      (let ((image-type (image-type-from-data gopher-current-data)))
+        (if image-type
+            (progn
+              (insert "     ")
+              (insert-image (create-image
+                             gopher-current-data image-type 'data))
+              (gopher-finish-buffer))
+          (error (format "Could not determine image type for %s" gopher-current-address)))))))
+
+(defun gopher-finish-buffer ()
+  (setq buffer-read-only t)
+  (goto-char (point-min))
+  (gopher-remove-dos-eol)
+  (message "Loaded %s." (gopher-format-address gopher-current-address)))
+
+(defun gopher-goto-url-at-point (&optional arg)
+  (interactive)
+  (move-beginning-of-line nil)
+  (let* ((properties (text-properties-at (point)))
+        (content-type (gopher-get-content-type properties)))
+    (if (eq content-type 'search-query)
+        (call-interactively 'gopher-goto-search)
+      (gopher-goto-url (getf properties :hostname)
+                       (getf properties :selector)
+                       content-type))))
+
+(defun gopher-goto-parent (&optional arg)
+  (interactive)
+  (let* ((address gopher-current-address)
+         (url (nth 0 address))
+         (selector (nth 1 address)))
+  (gopher-goto-url url (gopher-selector-parent selector))))
+
+(defun gopher-goto-search (search-argument)
+  (interactive "MSearch argument: ")
+  (let* ((properties (text-properties-at (point)))
+         (content-type (gopher-get-content-type properties)))
+    (gopher-goto-url (getf properties :hostname)
+                     (getf properties :selector)
+                     content-type search-argument)))
+
+(define-derived-mode gopher-mode fundamental-mode "Gopher"
+  (set (make-local-variable 'gopher-current-data) nil)
+  (set (make-local-variable 'gopher-current-address) nil))
+
+(defvar gopher-current-data nil)
+(defvar gopher-current-address nil)
+
+(defalias 'gopher-next-line 'next-line)
+(defalias 'gopher-previous-line 'previous-line)
+
+(defun gopher-pop-last (list)
+  (remove-if (lambda (x) t) list :count 1 :from-end t))
+
+(defun gopher-selector-parent (selector)
+  (mapconcat 'identity (gopher-pop-last (split-string selector "/")) "/"))
+
+(defun w3m-open-this-url-in-gopher ()
+  "Open this URL in Gopher."
+  (interactive)
+  (gopher (w3m-anchor)))
+
+(defmacro gopher-navigate (direction content-type)
+  `(defun ,(intern (concat "gopher-" (symbol-name direction) "-" (symbol-name content-type))) ()
+     (interactive)
+     (,direction)
+     (move-beginning-of-line nil)
+     (while (not (eq ',content-type (gopher-get-content-type (text-properties-at (point)))))
+       (,direction))))
+
+(defun gopher-define-keymaps ()
+  (setq gopher-mode-map (make-sparse-keymap))
+  (define-key gopher-mode-map "\r" 'gopher-goto-url-at-point)
+  (define-key gopher-mode-map "n" 'gopher-next-line)
+  (define-key gopher-mode-map "p" 'gopher-previous-line)
+  (define-key gopher-mode-map "g" 'gopher)
+  (define-key gopher-mode-map "\t" (gopher-navigate next-line directory-listing))
+  (define-key gopher-mode-map "\M-\t" (gopher-navigate previous-line directory-listing))
+  (define-key gopher-mode-map "]" (gopher-navigate next-line plain-text))
+  (define-key gopher-mode-map "[" (gopher-navigate previous-line plain-text))
+  (define-key gopher-mode-map "u" 'gopher-goto-parent)
+  (define-key gopher-mode-map "r" 'gopher-refresh-current-address)
+  (define-key gopher-mode-map "q" 'quit-window))
+
+(gopher-define-keymaps)
+
+(defun gopher-kill-address-at-point ()
+  (interactive)
+  (move-beginning-of-line nil)
+  (let* ((properties (text-properties-at (point)))
+         (string (mapconcat 'identity (list
+                                       (getf properties :hostname)
+                                       (getf properties :selector)) "/")))
+    (kill-new string)
+    (message string)))
+
+(provide 'gopher)
+
+;;; gopher.el ends here
